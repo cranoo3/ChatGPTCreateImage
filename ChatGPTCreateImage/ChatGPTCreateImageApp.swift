58742bdb9cf71a008112f439599e5f054f5c3fdb
cranoo3 <75974473+cranoo3@users.noreply.github.com>
2023-12-13 13:43:24 +0900

    Initial Commit

---
+++ ChatGPTCreateImage/ChatGPTCreateImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTCreateImageApp.swift
+//  ChatGPTCreateImage
+//
+//  Created by cranoo3 on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTCreateImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPTCreateImage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatGPTCreateImage
+//
+//  Created by cranoo3 on 2023/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
